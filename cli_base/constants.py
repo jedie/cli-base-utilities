7cee62e400bd80e51f206f2b6e3ce910a62e29c2
JensDiemer <git@jensdiemer.de>
2023-08-09 16:33:57 +0200

    Project setup updates

--- cli_base/constants.py
+++ cli_base/constants.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 import cli_base
@@ -6,3 +7,4 @@ import cli_base
 CLI_EPILOG = 'Project Homepage: https://github.com/jedie/cli-base-utilities'
 
 BASE_PATH = Path(cli_base.__file__).parent
+PY_BIN_PATH = Path(sys.executable).parent
