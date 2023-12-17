76cbf98109303727f72b9a17dd86b111c063afbc
Jens Diemer <git@jensdiemer.de>
2023-12-17 12:32:15 +0100

    Bugfix .pre-commit-config.yaml

--- cli_base/tests/test_pre_commit.py
+++ cli_base/tests/test_pre_commit.py
@@ -4,7 +4,9 @@ from unittest import TestCase
 from bx_py_utils.path import assert_is_file
 from pre_commit import clientlib
 
+import cli_base
 from cli_base.cli.dev import PACKAGE_ROOT
+from cli_base.cli_tools.constants import GITHUB_ACTION
 from cli_base.cli_tools.subprocess_utils import ToolsExecutor
 
 
@@ -13,22 +15,35 @@ class PreCommitTests(TestCase):
         file_path = PACKAGE_ROOT / '.pre-commit-hooks.yaml'
         assert_is_file(file_path)
 
-        hooks = clientlib.load_manifest(file_path)
+        hooks = clientlib.load_manifest(file_path)  # Will only pass, if YAML file is valid.
 
-        tools_executor = ToolsExecutor(cwd=PACKAGE_ROOT)
+        # We should not block merge requests because the execution of the hook may fail,
+        # because after a release the history may be "changed" because of version bump.
+        if not GITHUB_ACTION:
+            tools_executor = ToolsExecutor(cwd=PACKAGE_ROOT)
 
-        checked_ids = []
-        for hook in hooks:
-            entry = hook['entry']
-            popenargs = shlex.split(entry)
-            tools_executor.verbose_check_call(*popenargs)
-            checked_ids.append(hook['id'])
+            checked_ids = []
+            for hook in hooks:
+                entry = hook['entry']
+                popenargs = shlex.split(entry)
+                tools_executor.verbose_check_call(*popenargs)
+                checked_ids.append(hook['id'])
 
-        checked_ids.sort()
-        self.assertEqual(checked_ids, ['update-readme-history'])
+            checked_ids.sort()
+            self.assertEqual(checked_ids, ['update-readme-history'])
 
     def test_pre_commit_config_file(self):
         file_path = PACKAGE_ROOT / '.pre-commit-config.yaml'
         assert_is_file(file_path)
 
-        clientlib.load_config(file_path)
+        config = clientlib.load_config(file_path)  # Will only pass, if YAML file is valid.
+        self.assertEqual(
+            config['repos'],
+            [
+                {
+                    'hooks': [{'id': 'update-readme-history'}],
+                    'repo': 'https://github.com/jedie/cli-base-utilities',
+                    'rev': f'v{cli_base.__version__}',  # <<< Current Version **with** "v" prefix (as our git tags)
+                }
+            ],
+        )
