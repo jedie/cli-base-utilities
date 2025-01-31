jedie/cli-base

+lookup_python_tool()

cli_base/cli_tools/path_utils.py
@@ -8,6 +8,8 @@ from pathlib import Path
 from bx_py_utils.environ import OverrideEnviron
 from bx_py_utils.path import assert_is_file
 
+from cli_base.constants import PY_BIN_PATH
+
 
 logger = logging.getLogger(__name__)
 
@@ -68,3 +70,19 @@ def backup(file_path: Path, max_try=100) -> Path:
             shutil.copyfile(file_path, bak_file_candidate)
             return bak_file_candidate
     raise RuntimeError('No backup made: Maximum attempts to find a file name failed.')
+
+
+def lookup_python_tool(tool_name: str, mode=os.F_OK | os.X_OK, bin_path: Path | None = None) -> Path | None:
+    # If a virtualenv is use, check first in this bin path:
+    bin_path = bin_path or PY_BIN_PATH
+    bin_path_str = str(bin_path)
+    if bin_path_str not in os.environ['PATH']:
+        if tool_path := shutil.which(tool_name, mode=mode, path=bin_path_str):
+            logger.debug('%r found in: %s', tool_name, tool_path)
+            return Path(tool_path)
+
+    if tool_path := shutil.which(tool_name, mode=mode):
+        logger.debug('%r found in PATH: %s', tool_name, tool_path)
+        return Path(tool_path)
+
+    logger.debug('%r not found!', tool_name)

cli_base/cli_tools/tests/test_path_utils.py
@@ -1,10 +1,12 @@
+import os
 import tempfile
 from pathlib import Path
 from unittest import TestCase
 
-from cli_base.cli_tools.path_utils import backup, expand_user
+from cli_base.cli_tools.path_utils import backup, expand_user, lookup_python_tool
 from cli_base.cli_tools.test_utils.environment_fixtures import AsSudoCallOverrideEnviron
 from cli_base.cli_tools.test_utils.logs import AssertLogs
+from cli_base.constants import PY_BIN_PATH
 
 
 class PathUtilsTestCase(TestCase):
@@ -56,3 +58,41 @@ class PathUtilsTestCase(TestCase):
             with self.assertRaises(RuntimeError) as cm:
                 backup(test_path, max_try=3)
             self.assertEqual(cm.exception.args, ('No backup made: Maximum attempts to find a file name failed.',))
+
+    def test_lookup_python_tool(self):
+        with tempfile.TemporaryDirectory(prefix='test_') as temp_dir:
+            temp_path = Path(temp_dir)
+
+            UNIQUE_FILE_NAME = 'this-is-the-test-tool'
+            self.assertIs(lookup_python_tool(UNIQUE_FILE_NAME), None)
+
+            fake_tool_path = temp_path / UNIQUE_FILE_NAME
+            fake_tool_path.touch(mode=0o777)
+            self.assertIs(lookup_python_tool(UNIQUE_FILE_NAME), None)
+            self.assertEqual(
+                lookup_python_tool(tool_name=UNIQUE_FILE_NAME, bin_path=temp_path),
+                fake_tool_path,
+            )
+
+            # Must be executable as default:
+            fake_tool_path.chmod(0o666)
+            self.assertIs(
+                lookup_python_tool(tool_name=UNIQUE_FILE_NAME, bin_path=temp_path),
+                None,
+            )
+
+            # Or just a file:
+            self.assertEqual(
+                lookup_python_tool(tool_name=UNIQUE_FILE_NAME, mode=os.F_OK, bin_path=temp_path),
+                fake_tool_path,
+            )
+
+        python_path = lookup_python_tool('python')
+        self.assertIsInstance(python_path, Path)
+        self.assertEqual(python_path.name, 'python')
+        self.assertEqual(python_path.parent, PY_BIN_PATH)  # our venv interpreter?
+
+        # "ls" command should be available on all systems, isn't it?
+        tool_path = lookup_python_tool('ls')
+        self.assertIsInstance(tool_path, Path)
+        self.assertEqual(tool_path.name, 'ls')

jedie/cli-base-utilities

cli_base/cli_tools/path_utils.py
@@ -8,6 +8,8 @@ from pathlib import Path
 from bx_py_utils.environ import OverrideEnviron
 from bx_py_utils.path import assert_is_file
 
+from cli_base.constants import PY_BIN_PATH
+
 
 logger = logging.getLogger(__name__)
 
@@ -68,3 +70,22 @@ def backup(file_path: Path, max_try=100) -> Path:
             shutil.copyfile(file_path, bak_file_candidate)
             return bak_file_candidate
     raise RuntimeError('No backup made: Maximum attempts to find a file name failed.')
+
+
+def which(tool_name: str, mode=os.F_OK | os.X_OK, path: Path | None = None) -> Path | None:
+    """
+    A shutil.which() that will look into a virtualenv bin path first.
+    """
+    # If a virtualenv is use, check first in this bin path:
+    path = path or PY_BIN_PATH
+    bin_path_str = str(path)
+    if bin_path_str not in os.environ['PATH']:
+        if tool_path := shutil.which(tool_name, mode=mode, path=bin_path_str):
+            logger.debug('%r found in: %s', tool_name, tool_path)
+            return Path(tool_path)
+
+    if tool_path := shutil.which(tool_name, mode=mode):
+        logger.debug('%r found in PATH: %s', tool_name, tool_path)
+        return Path(tool_path)
+
+    logger.debug('%r not found!', tool_name)

cli_base/cli_tools/tests/test_path_utils.py
@@ -1,10 +1,12 @@
+import os
 import tempfile
 from pathlib import Path
 from unittest import TestCase
 
-from cli_base.cli_tools.path_utils import backup, expand_user
+from cli_base.cli_tools.path_utils import backup, expand_user, which
 from cli_base.cli_tools.test_utils.environment_fixtures import AsSudoCallOverrideEnviron
 from cli_base.cli_tools.test_utils.logs import AssertLogs
+from cli_base.constants import PY_BIN_PATH
 
 
 class PathUtilsTestCase(TestCase):
@@ -56,3 +58,41 @@ class PathUtilsTestCase(TestCase):
             with self.assertRaises(RuntimeError) as cm:
                 backup(test_path, max_try=3)
             self.assertEqual(cm.exception.args, ('No backup made: Maximum attempts to find a file name failed.',))
+
+    def test_which(self):
+        with tempfile.TemporaryDirectory(prefix='test_') as temp_dir:
+            temp_path = Path(temp_dir)
+
+            UNIQUE_FILE_NAME = 'this-is-the-test-tool'
+            self.assertIs(which(UNIQUE_FILE_NAME), None)
+
+            fake_tool_path = temp_path / UNIQUE_FILE_NAME
+            fake_tool_path.touch(mode=0o777)
+            self.assertIs(which(UNIQUE_FILE_NAME), None)
+            self.assertEqual(
+                which(tool_name=UNIQUE_FILE_NAME, path=temp_path),
+                fake_tool_path,
+            )
+
+            # Must be executable as default:
+            fake_tool_path.chmod(0o666)
+            self.assertIs(
+                which(tool_name=UNIQUE_FILE_NAME, path=temp_path),
+                None,
+            )
+
+            # Or just a file:
+            self.assertEqual(
+                which(tool_name=UNIQUE_FILE_NAME, mode=os.F_OK, path=temp_path),
+                fake_tool_path,
+            )
+
+        python_path = which('python')
+        self.assertIsInstance(python_path, Path)
+        self.assertEqual(python_path.name, 'python')
+        self.assertEqual(python_path.parent, PY_BIN_PATH)  # our venv interpreter?
+
+        # "ls" command should be available on all systems, isn't it?
+        tool_path = which('ls')
+        self.assertIsInstance(tool_path, Path)
+        self.assertEqual(tool_path.name, 'ls')